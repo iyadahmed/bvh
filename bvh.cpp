71fd11d2c1adf86996a400aff806572c944f7508
Iyad Ahmed <iyadahmed430@gmail.com>
2022-12-17 12:26:28 +0200

    Build BVH tree nodes

--- bvh.cpp
+++ bvh.cpp
@@ -1,4 +1,5 @@
 #include <algorithm> // for std::partition
+#include <assert.h>
 #include <iostream>
 #include <limits>
 #include <stdio.h>
@@ -17,8 +18,60 @@ struct Triangle {
     }
 };
 
-static void bvh(std::vector<Triangle>::iterator begin, std::vector<Triangle>::iterator end)
+struct Node {
+    std::vector<Triangle>::iterator begin, end;
+    Node *left = nullptr, *right = nullptr;
+
+    Node(std::vector<Triangle>::iterator begin, std::vector<Triangle>::iterator end)
+    {
+        this->begin = begin;
+        this->end = end;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    bool is_leaf() const
+    {
+        return (left == nullptr) && (right == nullptr);
+    }
+};
+
+static int count_nodes(Node* node)
 {
+    if (node == nullptr)
+        return 0;
+
+    return 1 + count_nodes(node->left) + count_nodes(node->right);
+}
+
+// https://stackoverflow.com/a/9181223/8094047
+static void free_tree(Node* node)
+{
+    if (node == nullptr)
+        return;
+
+    free_tree(node->left);
+    free_tree(node->right);
+
+    delete node;
+}
+
+static int count_leaf_triangles(Node* node)
+{
+    if (node == nullptr) {
+        return 0;
+    } else if (node->is_leaf()) {
+        return std::abs(std::distance(node->begin, node->end));
+    } else {
+        return count_leaf_triangles(node->left) + count_leaf_triangles(node->right);
+    }
+}
+
+static void bvh(Node* parent)
+{
+    auto begin = parent->begin;
+    auto end = parent->end;
+
     if (begin == end)
         return;
 
@@ -54,8 +107,14 @@ static void bvh(std::vector<Triangle>::iterator begin, std::vector<Triangle>::it
         return;
     }
 
-    bvh(begin, middle);
-    bvh(middle, end);
+    Node* left = new Node(begin, middle);
+    Node* right = new Node(middle, end);
+
+    parent->left = left;
+    parent->right = right;
+
+    bvh(left);
+    bvh(right);
 }
 
 int main(int argc, char* argv[])
@@ -76,7 +135,13 @@ int main(int argc, char* argv[])
 
     std::cout << "Number of triangles: " << tris.size() << std::endl;
 
-    bvh(tris.begin(), tris.end());
+    Node* root = new Node(tris.begin(), tris.end());
+
+    bvh(root);
+
+    assert(count_leaf_triangles(root) == tris.size());
+
+    free_tree(root);
 
     return 0;
 }
