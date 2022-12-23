25796b186392e70393812de12cf29cd76720fa86
Iyad Ahmed <iyadahmed430@gmail.com>
2022-12-23 20:36:49 +0200

    Implement camera movement and rotation via mouse and keyboard

---
+++ camera.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "vec4.hpp"
+
+class Camera {
+private:
+    Vector4 pos, up, right, forward;
+    float fov, pitch, yaw;
+public:
+    Camera() {
+        up = Vector4(0, 0, 1);
+        right = Vector4(1, 0, 0);
+        forward = up.cross3(right);
+        pos = Vector4(0, -5, -2);
+        fov = 3.14f / 4.0f;
+        pitch = 0.0f;
+        yaw = 0.0f;
+    }
+
+    void calc_vectors(Vector4 *p_up, Vector4 *p_right, Vector4 *p_forward) {
+        // Calculate new up and right vectors based on yaw
+        Vector4 new_forward = right * std::sin(yaw) + forward * std::cos(yaw);
+        Vector4 new_right = new_forward.cross3(up);
+
+        Vector4 new_up = new_forward * std::sin(pitch) + up * std::cos(pitch);
+        new_forward = new_up.cross3(new_right);
+
+        *p_forward = new_forward;
+        *p_up = new_up;
+        *p_right = new_right;
+    }
+
+    float get_fov() {
+        return fov;
+    }
+
+    Vector4 get_pos() {
+        return pos;
+    }
+
+    void rotate(float d_yaw, float d_pitch) {
+        yaw += d_yaw;
+        pitch += d_pitch;
+    }
+
+    void move(Vector4 vector) {
+        pos = pos + vector;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <cmath>
 #include <cstdio>
 #include <iostream>
 #include <stdexcept>
@@ -8,23 +9,52 @@
 
 #include "bvh.hpp"
 #include "vec4.hpp"
+#include "camera.hpp"
 
-static void render(SDL_Renderer* renderer, const BVH::BVH& bvh, Vector4 cam_pos, Vector4 p0, Vector4 p1, Vector4 p2)
+Camera cam;
+
+static void render(SDL_Renderer* renderer, const BVH::BVH& bvh)
 {
     int width, height;
     SDL_GetRendererOutputSize(renderer, &width, &height);
 
+    float fov = cam.get_fov();
+    float d = std::tan(fov / 2);
+    Vector4 cam_pos = cam.get_pos();
+    Vector4 up;
+    Vector4 right;
+    Vector4 forward;
+    cam.calc_vectors(&up, &right, &forward);
+
+    float aspect_ratio;
+    if (width > height) {
+        aspect_ratio = width / (float) height;
+    } else {
+        aspect_ratio = height / (float)width;
+    }
+
     auto t1 = std::chrono::high_resolution_clock::now();
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            Vector4 pixel_pos = cam_pos + p0 + (p1 - p0) * (x / (float)width) + (p2 - p0) * (y / (float)height);
+
+            float xn = x / (float)width;
+            xn = 2 * xn - 1;
+            xn *= aspect_ratio;
+            float yn = y / (float)height;
+            yn = 1 - 2 * yn;
+
+            Vector4 pixel_pos = cam_pos + forward + right * d * xn + up * d * yn;
 
             Vector4 ray_origin = cam_pos;
             Vector4 ray_direction = (pixel_pos - cam_pos).normalized3();
 
             float t = 0.0f;
-            if (bvh.does_intersect_ray(cam_pos, ray_direction, &t)) {
-                unsigned char c = 500 - (int)(t * 42);
+            if (bvh.does_intersect_ray(ray_origin, ray_direction, &t)) {
+                // Map t from [0, inf[ to [0, 1[
+                // https://math.stackexchange.com/a/3200751/691043
+                float tr = std::atan(t) / (3.14/2);
+                unsigned char c = (tr * tr) * 255;
+
                 SDL_SetRenderDrawColor(renderer, c, c, c, 255);
                 SDL_RenderDrawPoint(renderer, x, y);
             } else {
@@ -70,24 +100,58 @@ int main(int argc, char* argv[])
     SDL_Renderer* renderer;
     SDL_Window* window;
 
-    // FIXME: support non square aspect ratio
     constexpr int WINDOW_WIDTH = 640;
     constexpr int WINDOW_HEIGHT = 640;
+    constexpr float ROTATION_SPEED = .001;
+    constexpr float MOVEMENT_SPEED = .1;
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+    SDL_SetRelativeMouseMode(SDL_TRUE);
 
-    Vector4 cam_pos(-1.5f, -0.2f, 2.5);
-    Vector4 p0(-1, 1, -2), p1(1, 1, -2), p2(-1, -1, -2);
-    render(renderer, bvh, cam_pos, p0, p1, p2);
+    bool is_running = true;
+    int32_t dx, dy;
+    while (true) {
+        while (SDL_PollEvent(&event) != 0) {
+            Vector4 up;
+            Vector4 right;
+            Vector4 forward;
+            cam.calc_vectors(&up, &right, &forward);
 
-    SDL_RenderPresent(renderer);
-    while (1) {
-        if (SDL_PollEvent(&event) && event.type == SDL_QUIT) {
-            break;
+            switch (event.type) {
+            case SDL_QUIT:
+                is_running = false;
+                break;
+            case SDL_MOUSEMOTION:
+                dx = event.motion.xrel;
+                dy = event.motion.yrel;
+                cam.rotate(dx * ROTATION_SPEED, dy * ROTATION_SPEED);
+                break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE) { is_running = false; }
+                else if (event.key.keysym.sym == SDLK_w) {
+                    cam.move(forward * MOVEMENT_SPEED);
+                }
+                else if (event.key.keysym.sym == SDLK_s) {
+                    cam.move(forward * MOVEMENT_SPEED * -1);
+                }
+                else if (event.key.keysym.sym == SDLK_a) {
+                    cam.move(right * MOVEMENT_SPEED * -1);
+                }
+                else if (event.key.keysym.sym == SDLK_d) {
+                    cam.move(right * MOVEMENT_SPEED);
+                }
+                break;
+            }
         }
+
+        if (!is_running)
+            break;
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        render(renderer, bvh);
+        SDL_RenderPresent(renderer);
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
